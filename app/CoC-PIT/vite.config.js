ea24d1baf6b60d8f4451930b4f3dfef77e3798d4
Nevin McIntyre <NevinMcIntyreGIS@gmail.com>
2026-01-03 11:50:43 -0800

    fix vite config

--- app/CoC-PIT/vite.config.js
+++ app/CoC-PIT/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/COC-PIT/",
+  base: "/CoC-PIT/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
